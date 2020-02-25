3b0b6dff1676d812878c5a1d32c6cc14c69dd6ba
ILIA <60580096+Raven164@users.noreply.github.com>
2020-02-25 21:17:03 +0400

    feat:add task algorithm
    feat:add task algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+	if(matrix == undefined){
+    return [];
+    }
+    else{
+    var args = [];
+    for (var i = 0; i < matrix.length; i++) {
+     ((i+1) % 2 == 0) ? args = args
+     .concat((matrix[i]).sort((a,b) => b - a)) 
+     : args = args
+     .concat(matrix[i].sort((a,b) => a - b));
+    }
+    return args;
+    }
+  }	
+
+
